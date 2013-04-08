ac71b0ce0447cf7623bcf690c48b8f320015c63a
yuzhanwu <yuzw@witmob.com>
2013-04-08 15:42:01 +0800

    git add run.js

---
+++ run.js
@@ -0,0 +1,11 @@
+var server = require("./server");
+var router = require("./router");
+var requestHandlers = require("./requestHandler");
+
+var handle = {}
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.show;
+
+server.start(router.route, handle);
